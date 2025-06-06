9f0b52ddb3ba2dba687f389c3cff9c5dce7a55bf
Tim S. <pcstoepsel@gmail.com>
2025-06-06 17:48:18 +0200

    websocket

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3001, host: '0.0.0.0' });
+let time = 0;
 
 let isStarted = false;
+let timerInterval = null;
+
+function startTimer(duration) {
+  time = duration;
+  timerInterval = setInterval(() => {
+    if (isStarted && time > 0) {
+      time--;
+      wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: 'time', value: time }));
+        }
+      });
+      if (time === 0) {
+        isStarted = false;
+        clearInterval(timerInterval);
+        wss.clients.forEach(client => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send('stop');
+          }
+        });
+      }
+    }
+  }, 1000*60);
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null;
+}
 
 wss.on('connection', (ws) => {
   console.log('Client verbunden');
   if (isStarted) {
     ws.send('started');
+    ws.send(JSON.stringify({ type: 'time', value: time }));
+
   }
 
   ws.on('message', (message) => {
@@ -21,8 +53,12 @@ wss.on('connection', (ws) => {
       wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
           client.send('start');
+          ws.send(JSON.stringify({ type: 'time', value: time }));
         }
       });
+      if(!timerInterval) {
+        startTimer(60)
+      }
     } else if (message.toString() === 'stop') {
       // Sende an alle Clients
       isStarted = false;
@@ -32,6 +68,7 @@ wss.on('connection', (ws) => {
           client.send('stop');
         }
       });
+      stopTimer();
     }
   });
 });
